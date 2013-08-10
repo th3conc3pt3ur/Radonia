013bc987e96576739e8c5c5f7aa9c8f3e8090061
Quentin BAZIN <quent42340@gmail.com>
2013-08-10 15:36:56 +0200

    Fixing heart display bug.

--- source/character.cpp
+++ source/character.cpp
@@ -89,7 +89,7 @@ Character::Character(char *filename, CharacterType type, s16 x, s16 y, u8 direct
 	m_canTurn = true;
 	
 	// FIXME: TEMP
-	m_lifes = 32;
+	m_lifes = 27;
 	m_maxLifes = 32;
 }
 
@@ -214,6 +214,10 @@ void Character::move() {
 	CollisionManager::doorCollisions(this);
 	CollisionManager::testCollisions(this);
 	
+	if(m_collidedCharacter && m_collidedCharacter->isMonster() && isPlayer()) {
+		hurt();
+	}
+	
 	// Move character
 	m_x += m_vx * CHARACTER_SPEED;
 	m_y += m_vy * CHARACTER_SPEED;
@@ -222,83 +226,80 @@ void Character::move() {
 	m_vx = 0;
 	m_vy = 0;
 }
+
 void Character::render() {
 	if(m_moving) playAnimation(m_x, m_y, m_direction);
 	else drawFrame(m_x, m_y, m_direction);
 }
 
 void Character::hurt() {
-	// TODO with new collision states
-	/*if((collidedSprite && (collidedSprite->isPWeapon() && isMonster()))
-	|| (collidedSprite && ((collidedSprite->isMWeapon() || collidedSprite->isMonster()) && isPlayer()))) {
-		if(m_hurtTimer.time() - m_timerLastValue > 5) {
-			if(isPlayer()) {
-				// Block comamnds
-				m_blockedCommands = true;
-			} else {
-				// Stop monster movement
-				m_movementTimer.stop();
-			}
-			
-			// Change sprite texture
-			sf::Color c = m_spr.getColor();
-			m_spr.setColor(invertColor(c));
-			
-			// Get sprite direction vectors
-			s8 e_x = m_x - collidedCharacter->x();
-			s8 e_y = m_y - collidedCharacter->y();
-			
-			// Set movement vectors
-			if(!collidedTile && abs(e_x) > 8) m_vx = (e_x==0)?0:((e_x<0)?-2:2);
-			if(!collidedTile && abs(e_y) > 8) m_vy = (e_y==0)?0:((e_y<0)?-2:2);
-			
-			// Temporary collision states
-			Character *tmpCollidedCharacter = collidedCharacter;
-			Weapon *tmpCollidedWeapon = collidedWeapon;
-			
-			// Reset collision states
-			collidedCharacter = NULL;
-			collidedWeapon = NULL;
-			
-			// Setup temporary movement vectors
-			s8 t_vx = m_vx;
-			s8 t_vy = m_vy;
-			
-			// Test collisions
-			testCollisions();
-			
-			// Reset movement vectors
-			m_vx = t_vx;
-			m_vy = t_vy;
-			
-			// Reset collision states with temp values
-			collidedCharacter = tmpCollidedCharacter;
-			collidedWeapon = tmpCollidedWeapon;
-			
-			// Reset timer last value
-			m_timerLastValue = m_hurtTimer.time();
-			
-			// Reset collided sprite and blocked commands states
-			if(abs(e_x) > 32 || abs(e_y) > 32 || collidedTile) {
-				collidedCharacter = NULL;
-				collidedWeapon = NULL;
-				if(isPlayer()) m_blockedCommands = false;
-				else		   m_movementTimer.start();
-				m_spr.setColor(m_defaultColor);
-			}
+	if(m_hurtTimer.time() - m_timerLastValue > 5) {
+		if(isPlayer()) {
+			// Block comamnds
+			m_canMove = false;
+		} else {
+			// Stop monster movement
+			m_movementTimer.stop();
 		}
 		
-		if(m_hurtTimer.time() > 500) {
-			// Hurt sprite
-			m_lifes--;
-			
-			// Reset timer
-			m_hurtTimer.reset();
-			m_hurtTimer.start();
-			
-			// Reset timer last value
-			m_timerLastValue = m_hurtTimer.time();
+		// Change sprite texture
+		sf::Color c = m_spr.getColor();
+		m_spr.setColor(invertColor(c));
+		
+		// Get character direction vectors
+		s8 e_x = m_x - m_collidedCharacter->x();
+		s8 e_y = m_y - m_collidedCharacter->y();
+		
+		// Set movement vectors
+		if(!m_collidedTile && abs(e_x) > 8) m_vx = (e_x==0)?0:((e_x<0)?-2:2);
+		if(!m_collidedTile && abs(e_y) > 8) m_vy = (e_y==0)?0:((e_y<0)?-2:2);
+		
+		// Temporary collision states
+		Character *tmpCollidedCharacter = m_collidedCharacter;
+		//Weapon *tmpCollidedWeapon = m_collidedWeapon;
+		
+		// Reset collision states
+		m_collidedCharacter = NULL;
+		//m_collidedWeapon = NULL;
+		
+		// Setup temporary movement vectors
+		s8 t_vx = m_vx;
+		s8 t_vy = m_vy;
+		
+		// Test collisions
+		CollisionManager::testCollisions(this);
+		
+		// Reset movement vectors
+		m_vx = t_vx;
+		m_vy = t_vy;
+		
+		// Reset collision states with temp values
+		m_collidedCharacter = tmpCollidedCharacter;
+		//m_collidedWeapon = tmpCollidedWeapon;
+		
+		// Reset timer last value
+		m_timerLastValue = m_hurtTimer.time();
+		
+		// Reset collided sprite and blocked commands states
+		if(abs(e_x) > 32 || abs(e_y) > 32 || m_collidedTile) {
+			m_collidedCharacter = NULL;
+			//m_collidedWeapon = NULL;
+			if(isPlayer()) m_canMove = true;
+			else		   m_movementTimer.start();
+			m_spr.setColor(m_defaultColor);
 		}
-	}*/
+	}
+	
+	if(m_hurtTimer.time() > 500) {
+		// Hurt sprite
+		m_lifes--;
+		
+		// Reset timer
+		m_hurtTimer.reset();
+		m_hurtTimer.start();
+		
+		// Reset timer last value
+		m_timerLastValue = m_hurtTimer.time();
+	}
 }
 
